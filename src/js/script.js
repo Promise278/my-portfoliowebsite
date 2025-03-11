27ff7d9f83900118b49966712653348a0a8fb3e1
Promise278 <promiseobi2007@gmail.com>
2025-03-11 16:07:35 +0100

    added responsiveness

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,12 @@ let menuButton = document.getElementById("menu-open");
 menuButton.onclick = () => {
     menuButton.name = menuButton.name === "menu" ? "close" : "menu";
 };
+
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open')
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+})
